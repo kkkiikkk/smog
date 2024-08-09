a8bca8c6cce97378bbb1cfce2b12ae10f3adb7f6
kkkiikkk <ruddnovskiy@gmail.com>
2024-08-09 15:03:24 +0300

    feat: recaptcha setup

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,12 +5,29 @@ class CommentsController < ApplicationController
     @comment = @image.comments.build(comment_params)
     @comment.user = current_user
 
-    if @comment.save
-      redirect_to category_image_path(@category, @image)
+    success = verify_recaptcha(action: 'add_comment', minimum_score: 0)
+    checkbox_success = verify_recaptcha(model: @comment, site_key: '6Lc9DCMqAAAAACjwoZKeJA6OfIkohwD6FbkFgxpq') unless success
+    Rails.logger.warn(checkbox_success)
+    Rails.logger.warn(success)
+    if checkbox_success || success
+      @comment.save
+        redirect_to category_image_path(@category, @image)
+      # else
+        # render json: { error: 'Error creating comment.' }, status: :unprocessable_entity
+      # end 
     else
-      render json: { error: 'Error creating comment.' }, status: :unprocessable_entity
+        if !success
+          @show_checkbox_recaptcha = true
+          Rails.logger.warn("Post not saved because of a recaptcha score of #{recaptcha_reply}")
+       end
+        # render :index, status: :unprocessable_entity
+        render json: { error: 'Error creating comment.' }, status: :unprocessable_entity
+
     end
-  end
+      
+    end
+  # end
+  # end
 
   private
 
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,4 @@
+Recaptcha.configure do |config|
+  config.site_key = '6Lc9DCMqAAAAACjwoZKeJA6OfIkohwD6FbkFgxpq'
+  config.secret_key = '6LftASMqAAAAAKqFTcN4MIzVoM6Oj8EJFG4lWtWJ'
+end
