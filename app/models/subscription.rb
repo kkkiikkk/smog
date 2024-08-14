1d9bb678ee9524f8e5b337594080afcba04f9a66
kkkiikkk <ruddnovskiy@gmail.com>
2024-08-14 15:54:56 +0300

    feat: add subscriptons and categories to admin panel

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[category user]
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[category_id created_at id id_value updated_at user_id]
+  end
 end
