18ddee2c6717b64fa8cb0f975bbc2f9fef305a21
jarvis152 <RostikBack@ribs.studio>
2024-08-19 13:21:59 +0300

    feat: add redirec to error's page

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,6 @@
 class Action < ApplicationRecord
+  validates :action_type, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["action_type", "created_at", "email", "id", "id_value", "timestamp", "updated_at", "url"]
   end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-
+  config.consider_all_requests_local = false
   # Devise
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
