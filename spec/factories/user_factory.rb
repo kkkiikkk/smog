44a095d1fee924852cfd38c1cd2339c6e3331706
jarvis152 <RostikBack@ribs.studio>
2024-08-19 10:47:41 +0300

    feat: add comment unit test

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    sequence(:id) { |n| n }
-    sequence(:email) { |n| "user#{n}@example.com" }
+    sequence(:id) { |n| rand(1..1000)  }
+    sequence(:email) { |n| "user#{rand(1..1000)}@example.com" }
     username { 'helo_username' }
     password { 'password123' }
     password_confirmation { "password123" }
