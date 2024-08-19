d1713a01b05cabf5785978c806ce0b292bb9a957
jarvis152 <RostikBack@ribs.studio>
2024-08-19 12:13:08 +0300

    feat: add action unit tests

---
+++ spec/models/action_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Action, type: :model do 
+  describe 'valid action' do   
+    subject(:action_test) { Action.new(action_type: action_type, email: 'test', url: 'test') }
+    
+    context 'when action is valid' do 
+      let(:action_type) { 'test_action' }
+
+      it 'returns true' do 
+        expect(action_test.valid?).to be(true)
+      end
+    end
+
+    context 'when action type is invalid' do 
+      let(:action_type) { nil }
+
+      it 'returns false' do 
+        expect(action_test.valid?).to be(false)
+      end
+    end
+  end
+end
