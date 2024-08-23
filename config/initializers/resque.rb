da33eb443294559670504409473ef9d854638905
kkkiikkk <ruddnovskiy@gmail.com>
2024-08-23 10:43:25 +0300

    feat: add redis env

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,8 @@ require 'resque'
 require 'resque/scheduler'
 require 'resque/scheduler/server'
 
-Resque.redis = Redis.new(host: 'redis', port: 6379)
+host = ENV['REDIS_HOST'] || 'localhost'
+port = ENV['REDIS_PORT'] || 6379
+
+Resque.redis = Redis.new(host: host, port: port)
 Resque.schedule = YAML.load_file(Rails.root.join('config', 'schedule.yml'))
