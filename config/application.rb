632f76c2a9e6be64604f57ed12dd6884578b8864
kkkiikkk <ruddnovskiy@gmail.com>
2024-08-27 16:06:18 +0300

    feat: updae application config

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module Smog
       g.template_engine :slim
     end
     config.active_job.queue_adapter = :resque
+    config.action_dispatch.cookies_same_site_protection = :none
+    config.action_controller.default_protect_from_forgery = false
     config.i18n.default_locale = :en
     config.i18n.available_locales = [:en, :ua]
     config.i18n.fallbacks = true
