b3fe7c801800d880d6e15f5fd828815579919329
kkkiikkk <ruddnovskiy@gmail.com>
2024-08-27 11:02:55 +0300

    fix: fix specefic image page test

--- spec/features/error_pages_spec.rb
+++ spec/features/error_pages_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.feature "Error Pages", type: :feature do
-  scenario "displays 404 error page" do
+RSpec.feature 'Error Pages', type: :feature do
+  scenario 'displays 404 error page' do
     visit '/non-existent-page'
 
     expect(page).to have_content('404')
-    expect(page).to have_content('The page were are looking for could not be found.')
+    expect(page).to have_content('The page you are looking for could not be found.')
   end
 end
